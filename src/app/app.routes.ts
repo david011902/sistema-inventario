00416d55ee6baa0f9794bdf37a9a525a623ff01f
David Acosta <davidlira0119@outlook.es>
2026-04-07 22:54:53 -0600

    add routes component sale

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
+import { SaleCreate } from './data/interfaces/sales/SaleCreate';
 import { Routes } from '@angular/router';
 import { ProductsComponent } from './modules/inventory/components/products/products';
 import { ProductDetail } from './modules/inventory/components/product-detail/product-detail';
 import { ProductCreate } from './modules/inventory/components/product-create/product-create';
 import { StocksComponent } from './modules/inventory/components/stocks/stocks';
 import { StockDetail } from './modules/inventory/components/stock-detail/stock-detail';
-import { Component } from '@angular/core';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'inventario', pathMatch: 'full' },
@@ -29,5 +29,30 @@ export const routes: Routes = [
     path: 'stock/editar/:id',
     loadComponent: () =>
       import('./modules/inventory/components/stock-edit/stock-edit').then((m) => m.StockEdit),
+  },
+  {
+    path: 'sales',
+    // Carga perezosa (Lazy Loading) - solo carga el código cuando el usuario entra
+    loadComponent: () =>
+      import('./modules/sales/components/sales/sales').then((m) => m.SalesComponent),
+  },
+  {
+    path: 'sales/crear',
+    loadComponent: () =>
+      import('./modules/sales/components/sale-create/sale-create').then(
+        (m) => m.SaleCreateComponent,
+      ),
+  },
+  {
+    path: 'sales/:folio',
+    loadComponent: () =>
+      import('./modules/sales/components/sale-detail/sale-detail').then(
+        (m) => m.SaleDetailComponent,
+      ),
+  },
+  {
+    path: 'sales/editar/:folio',
+    loadComponent: () =>
+      import('./modules/sales/components/sale-return/sale-return').then((m) => m.SaleReturn),
   },
 ];
