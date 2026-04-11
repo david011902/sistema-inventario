6672f85823749254f74c7c4daa23b3516b9c202e
David Acosta <davidlira0119@outlook.es>
2026-04-11 11:16:06 -0600

    add component login

---
+++ src/app/modules/login/login/login.ts
@@ -0,0 +1,62 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatOptionModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../core/auth/services/auth';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatAutocompleteModule,
+    MatProgressSpinner,
+  ],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
+})
+export class LoginComponent {
+  private fb = inject(FormBuilder);
+  private router = inject(Router);
+  private authService = inject(AuthService);
+  loading = signal(false);
+  errorMessage = signal<string | null>(null);
+  loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+  });
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+    this.loading.set(true);
+    this.errorMessage.set(null);
+    this.authService.login(this.loginForm.value).subscribe({
+      next: (response) => {
+        console.log('Login successful:', response);
+        // Handle successful login, e.g., navigate to dashboard
+        this.router.navigate(['/inventario']);
+      },
+      error: (error) => {
+        console.error('Login failed:', error);
+        // Handle login error, e.g., show error message
+      },
+    });
+  }
+}
