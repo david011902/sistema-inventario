66c4e2c03b9124be431e13f76544d723a76ed5c6
David Acosta <davidlira0119@outlook.es>
2026-04-11 11:16:21 -0600

    add auth routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,13 @@ import { ProductDetail } from './modules/inventory/components/product-detail/pro
 import { ProductCreate } from './modules/inventory/components/product-create/product-create';
 import { StocksComponent } from './modules/inventory/components/stocks/stocks';
 import { StockDetail } from './modules/inventory/components/stock-detail/stock-detail';
+import { authGuard } from './core/auth/guards/authGuard';
 
 export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./modules/login/login/login').then((m) => m.LoginComponent),
+  },
   { path: '', redirectTo: 'inventario', pathMatch: 'full' },
 
   { path: 'inventario', component: ProductsComponent },
@@ -16,6 +21,7 @@ export const routes: Routes = [
     path: 'inventario/editar/:id',
     loadComponent: () =>
       import('./modules/inventory/components/product-edit/product-edit').then((m) => m.ProductEdit),
+    canActivate: [authGuard],
   },
 
   { path: 'stock', component: StocksComponent },
@@ -23,18 +29,22 @@ export const routes: Routes = [
     path: 'stock/crear',
     loadComponent: () =>
       import('./modules/inventory/components/stock-create/stock-create').then((m) => m.StockCreate),
+    canActivate: [authGuard],
   },
   { path: 'stock/:id', component: StockDetail },
+
   {
     path: 'stock/editar/:id',
     loadComponent: () =>
       import('./modules/inventory/components/stock-edit/stock-edit').then((m) => m.StockEdit),
+    canActivate: [authGuard],
   },
   {
     path: 'sales',
     // Carga perezosa (Lazy Loading) - solo carga el código cuando el usuario entra
     loadComponent: () =>
       import('./modules/sales/components/sales/sales').then((m) => m.SalesComponent),
+    canActivate: [authGuard],
   },
   {
     path: 'sales/crear',
@@ -42,6 +52,7 @@ export const routes: Routes = [
       import('./modules/sales/components/sale-create/sale-create').then(
         (m) => m.SaleCreateComponent,
       ),
+    canActivate: [authGuard],
   },
   {
     path: 'sales/:folio',
@@ -49,10 +60,12 @@ export const routes: Routes = [
       import('./modules/sales/components/sale-detail/sale-detail').then(
         (m) => m.SaleDetailComponent,
       ),
+    canActivate: [authGuard],
   },
   {
     path: 'sales/editar/:folio',
     loadComponent: () =>
       import('./modules/sales/components/sale-return/sale-return').then((m) => m.SaleReturn),
+    canActivate: [authGuard],
   },
 ];
