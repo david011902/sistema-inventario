2db488442889b193f16dea8c55160dee1cd819c2
David Acosta <davidlira0119@outlook.es>
2026-04-14 17:07:01 -0600

    feat: implement dashboard feature

---
+++ src/app/modules/dashboard/inicio/inicio.ts
@@ -0,0 +1,48 @@
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
+import { ProductService } from '../../../core/services/productService';
+import { SaleService } from '../../../core/services/sale-service';
+import { ProductResponse } from '../../../data/interfaces/products/ProductResponse';
+import { SaleResponse } from '../../../data/interfaces/sales/SaleResponse';
+import { forkJoin } from 'rxjs';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { CurrencyPipe, DatePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-inicio',
+  imports: [MatCardModule, MatTableModule, MatButtonModule, DatePipe, CurrencyPipe, RouterLink],
+  templateUrl: './inicio.html',
+  styleUrl: './inicio.scss',
+})
+export class DashboardComponent implements OnInit {
+  private productService = inject(ProductService);
+  private salesService = inject(SaleService);
+  private cdr = inject(ChangeDetectorRef);
+  products: ProductResponse[] = [];
+  sales: SaleResponse[] = [];
+  isloadin = true;
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  private loadData() {
+    forkJoin({
+      product: this.productService.getProducts(),
+      sales: this.salesService.getSales(),
+    }).subscribe({
+      next: ({ product, sales }) => {
+        this.products = product;
+        this.sales = sales;
+        this.isloadin = false;
+        this.cdr.detectChanges();
+      },
+      error: (err) => console.error('Error cargando datos', err),
+    });
+  }
+  get totalRevenue() {
+    return this.sales.reduce((acc, sale) => acc + sale.total, 0);
+  }
+}
