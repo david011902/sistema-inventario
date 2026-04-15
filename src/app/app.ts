b7c8ecded37059dd82ce6617c1333e9ee19793ec
David Acosta <davidlira0119@outlook.es>
2026-04-14 21:59:24 -0600

    feat: conditionally hide bottom navigation in login route

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBottom } from './shared/components/nav-bottom/nav-bottom';
+import { AuthService } from './core/auth/services/auth';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,5 @@ import { NavBottom } from './shared/components/nav-bottom/nav-bottom';
 })
 export class App {
   protected readonly title = signal('sistema-inventario');
+  protected authService = inject(AuthService);
 }
