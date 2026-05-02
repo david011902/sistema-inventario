e44b7d9b5d1f34bb65489d123b861afd79007b8d
David Acosta <davidlira0119@outlook.es>
2026-05-01 23:50:11 -0600

    feat: add loading spinner to handle backend cold start

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBottom } from './shared/components/nav-bottom/nav-bottom';
 import { AuthService } from './core/auth/services/auth';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-root',
-  imports: [NavBottom, RouterOutlet],
+  imports: [NavBottom, RouterOutlet, MatProgressSpinnerModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
