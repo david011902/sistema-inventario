c7e46c4c971d10c2fbcf4d5782c18f14b636bd5e
David Acosta <davidlira0119@outlook.es>
2026-05-04 17:33:40 -0600

    fix: add loading spinner to handle backend cold start

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,9 @@
 // app.config.ts
-import { ApplicationConfig, inject, provideAppInitializer } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { catchError, of } from 'rxjs';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/auth/interceptor/authInterceptor';
-import { AuthService } from './core/auth/services/auth';
-
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor])),
-    provideAppInitializer(() => {
-      const authService = inject(AuthService);
-      return authService.validateSession().pipe(catchError(() => of(null)));
-    }),
-  ],
+  providers: [provideRouter(routes), provideHttpClient(withInterceptors([authInterceptor]))],
 };
