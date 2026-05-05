914170853aa96e2659506634cf5bbe36989796dc
David Acosta <davidlira0119@outlook.es>
2026-05-04 20:42:24 -0600

    feat: add confirm dialog in logout button

--- src/app/shared/components/nav-bottom/nav-bottom.ts
+++ src/app/shared/components/nav-bottom/nav-bottom.ts
@@ -4,6 +4,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 import { AuthService } from '../../../core/auth/services/auth';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialog } from '../confirm-dialog/confirm-dialog';
 
 @Component({
   selector: 'app-nav-bottom',
@@ -20,8 +22,21 @@ import { AuthService } from '../../../core/auth/services/auth';
 })
 export class NavBottom {
   private authService = inject(AuthService);
+  private dialog = inject(MatDialog);
 
   logout(): void {
-    this.authService.logout();
+    const dialogRef = this.dialog.open(ConfirmDialog, {
+      data: {
+        title: 'Cerrar sesión',
+        message: '¿Estás seguro de que deseas salir?',
+        confirmText: 'Salir',
+      },
+    });
+
+    dialogRef.afterClosed().subscribe((confirmed) => {
+      if (confirmed) {
+        this.authService.logout();
+      }
+    });
   }
 }
